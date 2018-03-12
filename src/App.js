1a9cb240359b8143d44dd6957704b157eed4122b
IFNET-FOR\ricardo.meira <ricardo.meira@ifactory.com.br>
2018-03-12 17:50:50 -0300

    person

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I am a react app</h1>
         <p>This is really working!</p>
-        <Person/>
+        <button>Switch Name</button>
+        <Person name="Max" age="28"/>
+        <Person name="Steve" age="12">My Hobbies: Racing</Person>
+        <Person name="Ethan" age="31"/>
       </div>
     );
   }
