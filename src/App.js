1572b49471ca149415aef537ea03c7663b583c39
IFNET-FOR\ricardo.meira <ricardo.meira@ifactory.com.br>
2019-02-20 16:30:54 -0300

    Error Boundary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -44,48 +45,40 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'ponter'
-    }
-
     let persons = null;
+    let btnClass = '';
 
     if(this.state.showPerson){
         persons = (
           <div>
             {this.state.persons.map((person, index) => {
-              return <Person 
-                click = {() => this.deletePersonHandler(index)} 
-                name = {person.name} 
-                age = {person.age}
-                key = {person.id}
-                changed = {(event) => this.nameChangedHandler(event, person.id)}/>
+              return <ErrorBoundary key = {person.id}>
+                <Person 
+                  click = {() => this.deletePersonHandler(index)} 
+                  name = {person.name} 
+                  age = {person.age}
+                  changed = {(event) => this.nameChangedHandler(event, person.id)}/>
+                </ErrorBoundary>
             })}
           </div>
         );
-
-        style.backgroundColor = 'red';
+        btnClass = classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if(this.state.persons.length <= 2){
-      classes.push('red');
+      assignedClasses.push(classes.red);
       if(this.state.persons.length <= 1){
-        classes.push('bold');
+        assignedClasses.push(classes.bold);
       }
     }
 
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I am a react app</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
+          <p className={assignedClasses.join(' ')}>This is really working!</p>
           <button 
-          style={style}
+          className={btnClass}
           onClick={() => this.togglePersonsHandle()}>Switch Name</button>
           {persons}
         </div>
