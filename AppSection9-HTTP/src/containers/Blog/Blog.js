abc455e03a803a115e4ebe4ec41e3d2bc74a52d6
IFNET-FOR\ricardo.meira <ricardo.meira@ifactory.com.br>
2019-08-02 13:35:51 -0300

    Lecture 197: Handling errors

--- AppSection9-HTTP/src/containers/Blog/Blog.js
+++ AppSection9-HTTP/src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount() {
@@ -22,6 +23,10 @@ class Blog extends Component {
             });
             this.setState({posts: updatedPosts});
             //console.log(response);
+        })
+        .catch(error => {
+            //console.log(error);
+            this.setState({error: true});
         });
     }
 
@@ -30,9 +35,12 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts.map((post) => {
-            return <Post key={post.id} clicked={() => this.postSelectedHandler(post.id)} title={post.title} author={post.author}/>
-        });
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>;
+        if(!this.state.error){
+            posts = this.state.posts.map((post) => {
+                return <Post key={post.id} clicked={() => this.postSelectedHandler(post.id)} title={post.title} author={post.author}/>
+            });
+        }
 
         return (
             <div>
