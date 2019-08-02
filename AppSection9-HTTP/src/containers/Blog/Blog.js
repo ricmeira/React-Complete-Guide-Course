c1dd8ce1f1fbe7efc9d27edccf5c6a3e345538a0
IFNET-FOR\ricardo.meira <ricardo.meira@ifactory.com.br>
2019-08-02 08:43:20 -0300

    Lectures 190/191: Gtting and showing posts

--- AppSection9-HTTP/src/containers/Blog/Blog.js
+++ AppSection9-HTTP/src/containers/Blog/Blog.js
@@ -7,19 +7,32 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts: []
+    }
+
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts').then(( response ) => {
-            console.log(response);
+            const posts = response.data.slice(0, 4);
+            const updatedPosts = posts.map(post => {
+                return {
+                    ...post, author: 'Max'
+                };
+            });
+            this.setState({posts: updatedPosts});
+            //console.log(response);
         });
     }
 
     render () {
+        const posts = this.state.posts.map((post) => {
+            return <Post key={post.id} title={post.title} author={post.author}/>
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
