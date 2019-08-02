5551190e3c09c17f1feb76a6342272f70a887be0
IFNET-FOR\ricardo.meira <ricardo.meira@ifactory.com.br>
2019-08-02 08:48:39 -0300

    Lecture 192: Post selectable

--- AppSection9-HTTP/src/containers/Blog/Blog.js
+++ AppSection9-HTTP/src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount() {
@@ -24,9 +25,13 @@ class Blog extends Component {
         });
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
     render () {
         const posts = this.state.posts.map((post) => {
-            return <Post key={post.id} title={post.title} author={post.author}/>
+            return <Post key={post.id} clicked={() => this.postSelectedHandler(post.id)} title={post.title} author={post.author}/>
         });
 
         return (
@@ -35,7 +40,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
