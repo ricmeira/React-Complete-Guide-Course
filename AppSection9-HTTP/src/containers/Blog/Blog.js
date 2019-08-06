d0bfd3805197b8481aeecfea2232ffe46b2312d8
Ricardo Meira <ricardogdmeira@gmail.com>
2019-08-06 13:45:59 -0300

    Lecture 223: Link to Switch Pages

--- AppSection9-HTTP/src/containers/Blog/Blog.js
+++ AppSection9-HTTP/src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
-import { Route }from 'react-router-dom';
+import { Route, Link }from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
@@ -15,8 +15,13 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href='/'>Home</a></li>
-                            <li><a href='/new-post'>New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li>
+                                <Link to={{
+                                    pathname: '/new-post',
+                                    hash: '#submit',
+                                    search: '?quick-submit=true'
+                                }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
