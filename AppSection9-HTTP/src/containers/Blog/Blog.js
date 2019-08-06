cab37b8495bf81d28878f13fead659b7468be3b4
Ricardo Meira <ricardogdmeira@gmail.com>
2019-08-06 13:39:08 -0300

    Lecture 221: Rendering components

--- AppSection9-HTTP/src/containers/Blog/Blog.js
+++ AppSection9-HTTP/src/containers/Blog/Blog.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 //import axios from 'axios';
 import { Route }from 'react-router-dom';
 
+import Posts from './Posts/Posts';
 import './Blog.css';
 
 class Blog extends Component {
@@ -18,8 +19,9 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact render={() => <h1>Home</h1>}/>
-                <Route path="/" render={() => <h1>Home2</h1>}/>
+                {/*<Route path="/" exact render={() => <h1>Home</h1>}/>
+                <Route path="/" render={() => <h1>Home2</h1>}/>*/}
+                <Route path="/" exact component={Posts}/>
             </div>
         );
     }
