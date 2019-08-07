d7c5b3f2da96241ec4a2a724542beecd0a90122f
Ricardo Meira <ricardogdmeira@gmail.com>
2019-08-07 15:15:48 -0300

    Lecture 239: Using the history prop to redirect

--- AppSection9-HTTP/src/containers/Blog/NewPost/NewPost.js
+++ AppSection9-HTTP/src/containers/Blog/NewPost/NewPost.js
@@ -24,13 +24,14 @@ class NewPost extends Component {
 
         axios.post('/posts', data).then( response => {
             console.log(response);
-            this.setState({submitted: true});
+            this.props.history.replace('/posts');
+            //this.setState({submitted: true});
         });
     }
     render () {
         let redirect = null;
         if(this.state.submitted){
-            <Redirect to="/posts" />
+            redirect = <Redirect to="/posts" />
         }
         return (
             <div className="NewPost">
