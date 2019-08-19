edf87b7a1f361fc73b89ccdce0fc3d60eacb1f47
Ricardo Meira <ricardogdmeira@gmail.com>
2019-08-19 09:09:30 -0300

    Lecture 321: Leaner switch case

--- redux--01-start/src/store/reducers/result.js
+++ redux--01-start/src/store/reducers/result.js
@@ -5,16 +5,20 @@ const initialState = {
     results: []
 }
 
+const deleteResult = (state, action) => {
+        //const id = 2;
+        //const updatedArray = [...state.results];
+        //updatedArray.splice(id, 2);
+        const updatedArray = state.results.filter(result => result.id !== action.resultElementId);
+        return updateObject(state, {results: updatedArray});
+};
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
             return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result})});
         case actionTypes.DELETE_RESULT:
-            //const id = 2;
-            //const updatedArray = [...state.results];
-            //updatedArray.splice(id, 2);
-            const updatedArray = state.results.filter(result => result.id !== action.resultElementId);
-            return updateObject(state, {results: updatedArray});
+            return deleteResult(state, action);
     }
     return state;
 };
