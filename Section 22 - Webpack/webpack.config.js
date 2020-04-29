baa545395cf2bcab99ec6a3b86732e6daf9ef6fc
Ricardo <ricardogdmeira@gmail.com>
2020-04-29 17:24:18 -0300

    Lecture 380: Loading CSS files

--- Section 22 - Webpack/webpack.config.js
+++ Section 22 - Webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
     mode: 'development',
@@ -14,6 +15,23 @@ module.exports = {
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {loader: 'style-loader'},
+                    {loader: 'css-loader', options: {
+                        importLoaders: 1,
+                        modules: {
+                            localIdentName: '[name]__[local]__[hash:base64:5]'
+                        }
+                    }},
+                    {loader: 'postcss-loader', options: {
+                        ident: 'postcss',
+                        pluguins: () => [autoprefixer()]
+                    }},
+                ],
                 exclude: /node_modules/
             }
         ]
