1942478151ec8a9023d05cd06553be2c634921c2
Ricardo <ricardogdmeira@gmail.com>
2020-05-06 17:07:17 -0300

    Lecture 387: Setting up React Project

---
+++ Section23-NextJS/pages/auth/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const authIndexPage = () => (
+    <div>
+        <h1>
+            The auth index page
+        </h1>
+    </div>
+);
+
+export default authIndexPage;
---
+++ Section23-NextJS/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const indexPage = () => (
+    <div>
+        <h1>
+            The main Page
+        </h1>
+    </div>
+);
+
+export default indexPage;
