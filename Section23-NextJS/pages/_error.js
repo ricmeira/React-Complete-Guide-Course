b61353f0829d70572e62e68a02850ceac901572c
Ricardo <ricardogdmeira@gmail.com>
2020-05-07 15:55:58 -0300

    Lecture 391: NextJS - Custom 404 page

---
+++ Section23-NextJS/pages/_error.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+import Router from 'next/router';
+
+const errorPage = () => (
+    <div>
+        <h1>
+            Oops, something went wrong
+        </h1>
+        <p>Try <Link href="/"><a>goind back</a></Link>.</p>
+    </div>
+);
+
+export default errorPage;
