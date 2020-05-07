e979b379dd81ee8bc2df8facf2b3e16410fc0884
Ricardo <ricardogdmeira@gmail.com>
2020-05-07 15:45:43 -0300

    Lecture 389: NextJS - Components and pages

--- Section23-NextJS/pages/auth/index.js
+++ Section23-NextJS/pages/auth/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import User from '../../components/User';
 
 const authIndexPage = () => (
     <div>
         <h1>
             The auth index page
         </h1>
+        <User name="Ricardo" age={28}/>
     </div>
 );
 
