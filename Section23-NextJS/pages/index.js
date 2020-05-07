09c43e765d5473a60848dcaeff7dae10e0ad0464
Ricardo <ricardogdmeira@gmail.com>
2020-05-07 15:36:27 -0300

    Lecture 388: NextJS - Understandig the basics

--- Section23-NextJS/pages/index.js
+++ Section23-NextJS/pages/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Link from 'next/link';
+import Router from 'next/router';
 
 const indexPage = () => (
     <div>
         <h1>
             The main Page
         </h1>
+        <p>Go to <Link href="auth"><a>Auth</a></Link></p>
+        <button onClick={() => Router.push('/auth')}>Go to auth</button>
     </div>
 );
 
