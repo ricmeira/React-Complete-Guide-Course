13bae4788f8b852960e0fac36682f35272efe9bf
Ricardo <ricardogdmeira@gmail.com>
2020-05-11 16:52:08 -0300

    Lecture 402: Animations - Wrapping the Transition component

--- Section24-Animations/src/App.js
+++ Section24-Animations/src/App.js
@@ -49,16 +49,7 @@ class App extends Component {
             </div>
           )}
         </Transition>
-        <Transition
-          in={this.state.modalIsOpen}
-          timeout={300}
-          mountOnEnter
-          unmountOnExit
-        >
-          {state => (
-            <Modal show={state} closed={this.closeModal} />
-          )}
-        </Transition>
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
         {this.state.modalIsOpen ? <Backdrop show /> : null}
         {/*<Modal show={this.state.modalIsOpen} closed={this.closeModal} />
         <Backdrop show={this.state.modalIsOpen} />*/}
