641235ce016f3ebef4ac8a0f7da9dba120a963b4
Ricardo <ricardogdmeira@gmail.com>
2020-07-03 15:33:00 -0300

    Lecture 474: ReactContext - Creating a concrete store

--- Section28-ReplacingReduxWithHooks/src/context/products-context.js
+++ Section28-ReplacingReduxWithHooks/src/context/products-context.js
@@ -1,56 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export const ProductsContext = React.createContext({
-    products: [],
-    toggleFavorite: id => {}
+  products: [],
+  toggleFavorite: (id) => {},
 });
 
-export default props => {
-    const [ products, setProducts ] = useState([
-        {
-          id: 'p1',
-          title: 'Red Scarf',
-          description: 'A pretty red scarf.',
-          isFavorite: false
-        },
-        {
-          id: 'p2',
-          title: 'Blue T-Shirt',
-          description: 'A pretty blue t-shirt.',
-          isFavorite: false
-        },
-        {
-          id: 'p3',
-          title: 'Green Trousers',
-          description: 'A pair of lightly green trousers.',
-          isFavorite: false
-        },
-        {
-          id: 'p4',
-          title: 'Orange Hat',
-          description: 'Street style! An orange hat.',
-          isFavorite: false
-        }
-    ]);
+export default (props) => {
+  const [products, setProducts] = useState([
+    {
+      id: "p1",
+      title: "Red Scarf",
+      description: "A pretty red scarf.",
+      isFavorite: false,
+    },
+    {
+      id: "p2",
+      title: "Blue T-Shirt",
+      description: "A pretty blue t-shirt.",
+      isFavorite: false,
+    },
+    {
+      id: "p3",
+      title: "Green Trousers",
+      description: "A pair of lightly green trousers.",
+      isFavorite: false,
+    },
+    {
+      id: "p4",
+      title: "Orange Hat",
+      description: "Street style! An orange hat.",
+      isFavorite: false,
+    },
+  ]);
 
-    const toggleFavorite = productId => {
-        setProducts(currentProducts => {
-            const prodIndex = currentProducts.findIndex(
-                p => p.id === productId
-              );
-              const newFavStatus = !currentProducts[prodIndex].isFavorite;
-              const updatedProducts = [...currentProducts];
-              updatedProducts[prodIndex] = {
-                ...currentProducts[prodIndex],
-                isFavorite: newFavStatus
-              };
-            return updatedProducts;
-        });
-    };
+  const toggleFavorite = (productId) => {
+    setProducts((currentProducts) => {
+      const prodIndex = currentProducts.findIndex((p) => p.id === productId);
+      const newFavStatus = !currentProducts[prodIndex].isFavorite;
+      const updatedProducts = [...currentProducts];
+      updatedProducts[prodIndex] = {
+        ...currentProducts[prodIndex],
+        isFavorite: newFavStatus,
+      };
+      return updatedProducts;
+    });
+  };
 
-    return (
-        <ProductsContext.Provider value={{ products, toggleFavorite }}>
-            {props.children}
-        </ProductsContext.Provider>
-    );
+  return (
+    <ProductsContext.Provider value={{ products, toggleFavorite }}>
+      {props.children}
+    </ProductsContext.Provider>
+  );
 };
